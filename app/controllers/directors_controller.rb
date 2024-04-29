4b7cc242a31aef7b1a6a8daba659f0be8762477b
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-29 05:06:06 +0000

    ADDED MOVIES

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,14 @@
 class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list"})
-
   end
+
+  def show
+    the_id = params.fetch("the_id")
+
+   matching_recotrds =  Director.where({ :id => the_id })
+
+   @the_director = matching_recotrds.at(0)
+    render({ :template => "director_templates/details"})
+  end 
 end
